da1139002f086325ea1bcfcb8cb74c3d19c9b3ce
Minseok Kim <comko123@naver.com>
2022-06-11 00:03:00 +0900

    Some modifications to the weather function

--- src/weather/WeatherComponents.js
+++ src/weather/WeatherComponents.js
@@ -8,7 +8,7 @@ export default function WeatherComponents(){
     const getLocation = () => {
         if (navigator.geolocation) { // GPS를 지원하면
           navigator.geolocation.getCurrentPosition((position) => {
-              console.log(position)
+              //console.log(position)
             setAdress(position.coords)
           },(error) => {
             console.error(error);
@@ -22,20 +22,20 @@ export default function WeatherComponents(){
         }
       }
       useEffect(()=>{getLocation()},[])
+
       const inputWeather = async() => {
 setUserWeather((await 
     axios(`https://api.openweathermap.org/data/2.5/onecall?lat=${adress.latitude}&lon=${adress.longitude}&appid=f980d31253eb2b185606cca64544373f&units=metric`))
 .data.hourly)
-setLoading(false);    
-}
-
+setLoading(false);  
+      }
     return(<>{loading?
     <>
     <h1>loading.....</h1>
     <button onClick={inputWeather}>click</button>
     </>
     :<> 
-    <h1>clear weather</h1>
+    <h1>Today weathers</h1>
         {userWeather.map((R)=>{
         return<WeatherDisplay 
         key={R.dt} 
@@ -44,8 +44,19 @@ setLoading(false);
         temp={R.temp}
         weather={R.weather}
         />})}
-
-    </>}
-   
+<button onClick={()=>{
+        const dateValue = new Date()
+        const  array =
+        userWeather.map((R)=>{ 
+          const value = new Date(R.dt*1000); 
+          return dateValue.getDate()!==value.getDate()?
+          null:R.temp })
+          const data = array.filter(E=>E!==null)
+          console.log("당일 최고온도" ,Math.max.apply(null,data))
+          console.log("당일 최저온도",Math.min.apply(null,data))
+          console.log(data)
+        }}
+        >click</button>
+    </>}  
     </>)
 }
--- src/weather/WeatherDisplay.js
+++ src/weather/WeatherDisplay.js
@@ -2,7 +2,8 @@ export default function Display(props){
         const dateValue = new Date()
         const value = new Date(props.dt*1000)
         const {humidity,temp,weather} = props
-     
+       //console.log(props)
+      
     return(
     <>
     {dateValue.getDate()!==value.getDate()?null:
@@ -12,7 +13,10 @@ export default function Display(props){
     ||value.getHours() ===23 ?
      <>
      <br/>
-     <h1>{value.getHours()} 시</h1>
+     <h1>{value.getHours()===10?
+     "아침":value.getHours()===16?
+     "점심":value.getHours()===20?
+     "저녁":"밤"} </h1>
      <h1>습도:{humidity}</h1>
      <h1>온도:{Math.round(temp)}</h1>
      {weather.map(R=>{return(
