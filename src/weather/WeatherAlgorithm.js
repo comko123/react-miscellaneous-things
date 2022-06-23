dd46d3702087424bd1c42b9182601474f10f27d8
Minseok Kim <comko123@naver.com>
2022-06-24 00:15:23 +0900

    WeatherAlgorithm Done :)

--- src/weather/WeatherAlgorithm.js
+++ src/weather/WeatherAlgorithm.js
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 
 const WeatherAlgorithm = (props) =>{
-    const {Htemp,Mtemp,outp} = props
+    const {Htemp,Mtemp,outp,rain} = props
 useEffect(()=>{
     console.log("당일 최고 온도: "+Math.round(Htemp)
     +" , "+"당일 최저 온도: "+Math.round(Mtemp)
-    +" , "+"겉옷 여부: "+outp)},[])
+    +" , "+"겉옷 여부: "+outp
+    +","+"강수여부: "+rain)},[])
     return <></>
 }
 export default WeatherAlgorithm;
--- src/weather/WeatherComponents.js
+++ src/weather/WeatherComponents.js
@@ -1,22 +1,11 @@
-import axios from "axios";
 import { useEffect ,useState } from "react";
-import WeatherAlgorithm from "./WeatherAlgorithm";
-import WeatherDisplay from "./WeatherDisplay";
+import InputWeather from "./InputWeather";
 export default function WeatherComponents(){
     const [latitude,setLatitude] = useState(0)
     const [longitude,setLongitude]=useState(0)
-    const [userWeather,setUserWeather] = useState([])
-    const [loading,setLoading] = useState(true);
-    const [highTemp,setHighTemp] = useState(0) 
-    const [lowTemp,setLowTemp] = useState(0)     
-    const [outerClothing,setOuterClothing] =useState(false)
-    
     const getLocation = () => {
-      if (navigator.geolocation) { // GPS를 지원하면
+      if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
-            //console.log(position.coords)
-             //console.log(navigator)
-          console.log(" 위도 : "+position.coords.latitude.toFixed(4)+" , 경도 : "+position.coords.longitude.toFixed(4))
           setLatitude(position.coords.latitude.toFixed(4))
           setLongitude(position.coords.longitude.toFixed(4))
         },(error) => {
@@ -30,58 +19,11 @@ export default function WeatherComponents(){
         alert('GPS를 지원하지 않습니다');
       }
     }
-    const inputWeather = async() => {  
-     setUserWeather((await 
-        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=0b9dc2c29f0c437b89527b6b12e02421&units=metric`))
-    .data.hourly)
-   }
-useEffect(()=>{inputWeather()},[])
-useEffect(()=>{getLocation()},[])
-           
-    const ClickEvent = () => {
-    
-      const dateValue = new Date()
-        const  array = 
-        userWeather.map((R)=>{ 
-          const value = new Date(R.dt*1000); 
-          return dateValue.getDate()!==value.getDate()?
-          null:R.temp })
-          const data = array.filter(E=>E!==null)
-          const hotdata = Math.max.apply(null,data)
-          setHighTemp(hotdata)
-          const colddata = Math.min.apply(null,data)
-          setLowTemp(colddata)
-          setLoading(false)
-          if(hotdata>=23||colddata<=12){setOuterClothing(true)}
-            else{setOuterClothing(false)}
 
-            const rainning =  
-            userWeather.map((R)=>{ 
-              const value = new Date(R.dt*1000); 
-              return dateValue.getDate()!==value.getDate()?
-              null:R.weather.map((K)=>K.main) })
-              const rainData = rainning.filter(E=>E!==null)
-          console.log(data) 
-          console.log(rainData)
-          console.log(rainData.map(Y=>Y==="Rain"?true:false))
-    }
-    
-    return(<>{loading?
-    <>
-    <h1>loading.....</h1>
-    <button onClick={ClickEvent}>click</button>
-    </>
-    :<> 
-    <h1>Today weathers</h1>
-        {userWeather.map((R)=>{
-        return<WeatherDisplay 
-        key={R.dt} 
-        dt={R.dt} 
-        humidity={R.humidity}
-        temp={R.temp}
-        weather={R.weather}
-        />})}
-       <WeatherAlgorithm Htemp = {highTemp} Mtemp={lowTemp} outp = {outerClothing}/>
-    </>}  
-    </>)
+useEffect(()=>{getLocation()},[])    
+
+    return(<>
+       <InputWeather latitude={latitude} longitude = {longitude}/>
+    </>  
+    )
 }
