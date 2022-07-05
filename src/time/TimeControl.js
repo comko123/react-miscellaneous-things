ab50a8f4e132ecb7166efd6c602f009339e1801b
Minseok Kim <comko123@naver.com>
2022-07-05 14:23:31 +0900

    ui time end :)

--- src/time/TimeControl.js
+++ src/time/TimeControl.js
@@ -1,30 +1,25 @@
-import { useCookies } from "react-cookie";
-import React, { useState } from "react";
-const TimeControl = () =>{
-   const [logo,setLogo] =useState(true)
-   const [cookies, setCookie] = useCookies([""]);
-   setTimeout(()=>{setLogo(false)},3000)
-   function Cksave (){
-      let now = new Date();
-      let after1m = new Date();
-      after1m.setMinutes(now.getMinutes() + 2); 
-      setCookie("remeberText", "Duration", { path: "/", expires: after1m });
-   }
-   Cksave()
-   
- return (
-    <>
-    {logo?
-    <>
-    <h1>weather coder</h1>
-    </>
-    :
-    <>
-    <h1>main page</h1>
-    <input type="button" value="Click" onClick={()=>{console.log(cookies)}}/>
-    </>
+import React,{useEffect, useState} from "react";
+const TimeControl = () => {
+
+const [logo,setLogo] = useState(true)
+
+const input = () =>{
+    if(sessionStorage.getItem("visit") === null){
+        setTimeout(()=>{setLogo(false)},3000)
+        console.log(logo)
+        if(logo){sessionStorage.setItem("visit","checked");console.log(logo)}
     }
-    </>
- )  
+    else{setLogo(false)}
 }
+useEffect(()=>{input()},[])
+    return(
+        <>
+        {(sessionStorage.getItem("visit") === null)?
+        <><h1>weather coder</h1></>
+        :
+        <><h1>main page</h1></>}        
+        </>
+    )
+}
+
 export default TimeControl;
